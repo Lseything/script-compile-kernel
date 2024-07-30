0bd3985813b5d989083f0e5fb80f1aa86cc89c28
Lseything <lseything@gmail.com>
2024-07-29 23:25:40 -0300

    script: reformulated script

---
+++ prepare-and-build-kernel.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Credits : @LSEYTHING ON TELEGRAM
+
+# Function to update and install packages
+install_packages() {
+    echo "Updating package list and installing necessary packages..."
+    sudo apt update
+    sudo apt upgrade -y
+    sudo apt install -y repo git rsync openssl gnutls-bin make
+}
+
+# Function to sync and download repository
+sync_repository() {
+    echo "Syncing repository..."
+    repo init -u https://android.googlesource.com/kernel/manifest -b common-android13-5.15
+    repo sync
+}
+
+# Function to download build.sh
+download_build_sh() {
+    echo "Downloading build.sh from host..."
+    cd your_kernel_folder
+    wget www.your-link-custom-build.sh
+}
+
+# Function to start the kernel build
+start_build() {
+    echo "Locating your_kernel_folder/build.sh..."
+    echo "Running build.sh..."
+    bash build.sh
+}
+
+# Main script execution
+main() {
+    install_packages
+    sync_repository
+    download_build_sh
+    start_build
+    echo "Your preparation and build finished."
+}
+
+main
